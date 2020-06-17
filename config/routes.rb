bc39ea4c2f037ffa11943b174095765e5f20a28e
Michael Inglis <michael@spinningcube.co.nz>
2020-06-17 23:37:15 +1200

    App working now with HTTP functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
+  post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at: '/cable'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "BigPurrsLuvva", password: "meow")
+User.create(username: "MeowMachine", password: "meow")
+User.create(username: "MeowyCat18", password: "meow")
